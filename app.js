8605580be26ea28f69bf51f92d2377cc5a508ffe
Jan <janx.fetalvero@gmail.com>
2022-09-12 21:23:01 +0800

    forms phase 1

---
+++ app.js
@@ -0,0 +1,9 @@
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm_password');
+
+if (password.value === '1') {
+    console.log('checking')
+}
+
+//todo
+// password/confirm password validation
