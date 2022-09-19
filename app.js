c02f52958b01e928a2837a83532c63ad4e41dee0
Jan <janx.fetalvero@gmail.com>
2022-09-20 06:04:15 +0800

    form phase 1.3

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm_password');
+const submit = document.getElementById('submit-button');
 
-console.log(password.value)
 //todo
-// password/confirm password validation
+// password/confirm password validation by clicking a button
+
+submit.addEventListener('click', (e) => {
+    if (password.value !== confirmPassword.value) {
+        alert('Password do not match')
+    }
+})
