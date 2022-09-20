aa7a5ac5a61be9c4e55266c9ece4a5f368771c8c
Jan <janx.fetalvero@gmail.com>
2022-09-20 21:37:09 +0800

    HTML form Final

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm_password');
 const submit = document.getElementById('submit-button');
 
-//todo
-// password/confirm password validation by clicking a button
-
+//this block of code will validate the password against the confirm pass word
 submit.addEventListener('click', (e) => {
     if (password.value !== confirmPassword.value) {
         alert('Password do not match')
